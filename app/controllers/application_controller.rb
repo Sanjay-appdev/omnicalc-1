a51adb7b4e7af2d9e083aee82fefe7867e3aad60
sanjay5649 <stuwrkr-smaru7@mx.uillinois.edu>
2023-03-02 05:13:14 +0000

    omnicalc half done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,23 @@ class ApplicationController < ActionController::Base
   def blank_square_form
 
     render({:template => "calculation_templates/square_form.html.erb"  })
-  end
-end 
+    end
+
+   def calculate_square
+
+    @num = params.fetch("elephant").to_i
+    
+    @square_of_num = @num ** 2
+
+   render({:template => "calculation_templates/square_results.html.erb"  })
+    end 
+    def calculate_random
+
+      @lower = params.fetch("user_min").to_f
+      @upper = params.fetch("user_max").to_f
+      @result = rand(@lower..@upper)
+
+      render({:template =>"calculation_templates/rand_results.html.erb" })
+    end
+   end
   
