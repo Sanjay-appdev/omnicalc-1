baaef1dab908878088da5d83f652df8dca3e9a16
sanjay5649 <stuwrkr-smaru7@mx.uillinois.edu>
2023-03-06 22:09:43 +0000

    Omnicalc is done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,29 +3,25 @@ class ApplicationController < ActionController::Base
     
 
     render({:template => "calculation_templates/square_form.html.erb"  })
-    end
+  end
 
-   def calculate_square
-    
-
+  def calculate_square
     @num = params.fetch("elephant").to_f
     
     @square_of_num = @num ** 2
 
-   render({:template => "calculation_templates/square_results.html.erb"  })
-    end 
+    render({:template => "calculation_templates/square_results.html.erb"  })
+  end 
 
-    def calculate_random
+  def calculate_random
 
-      @lower = params.fetch("user_min").to_f
-      @upper = params.fetch("user_max").to_f
-      @result = rand(@lower..@upper)
+    @lower = params.fetch("user_min").to_f
+    @upper = params.fetch("user_max").to_f
+    @result = rand(@lower..@upper)
 
-      render({:template =>      "calculation_templates/rand_results.html.erb" })
-    end
+    render({:template =>"calculation_templates/rand_results.html.erb" })
+  end
 
-    
-   
   def blank_payment_form 
 
     render({:template =>"calculation_templates/blank_payment_form.html.erb" })
@@ -45,19 +41,25 @@ class ApplicationController < ActionController::Base
     render({:template =>"calculation_templates/square_root_results.html.erb" })
   end
 
-end
-def calculate_payment
+  def payment_results
+    @jkl = params.fetch("user_apr").to_f
+    @shel = params.fetch("user_years").to_i
+    @fan = params.fetch("user_pv").to_f
+  
+    @shel_cal = @jkl /100 /12
+  
+    x = @shel_cal * @fan
+    y = 1-( (1 + @shel_cal)) ** - (@shel * 12)
+    @k = x / y
+  
+    render({:template =>"calculation_templates/hun_results.html.erb" })
+  
+  end
 
-  @jkl = params.fetch("apr_input").to_f
-  @shel = params.fetch("numyr_input").to_i
-  @fan = params.fetch("principal_input").to_f
 
-  @shel_cal = @jkl /100 /12
+  def blank_random_form
+    render({:template =>"calculation_templates/blank_random.html.erb" })
+  end
 
-  x = @shel_cal * @fan
-  y = 1-( (1 + @shel_cal)) ** - (@shel * 12)
-  @k = x / y
-
-  render({:template =>"calculation_templates/hun_results.html.erb" })
 
 end
